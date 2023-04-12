39c41fcc0cfacb497f62f7e63c28b279f0bc9569
Wallace Nascimento <wallacediogo10@hotmail.com>
2023-04-11 23:57:11 -0300

    feature: base created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [breakTime, setBreakTime] = useState(5);
+  const [sessionTime, setSessionTime] = useState(25);
+
+  function increaseBreakTime() {
+    setBreakTime(breakTime + 1)
+  }
+
+  function decreaseBreakTime() {
+    setBreakTime(breakTime - 1)
+  }
+
+  function increaseSessionTime() {
+    setSessionTime(sessionTime + 1)
+  }
+
+  function decreaseSessionTime() {
+    setSessionTime(sessionTime - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <h1>POMODORO</h1>
+      <div class="controls">
+        <div className="control-time">
+          <h3>session time</h3>
+          <button onClick={() => decreaseSessionTime()}>-</button>
+          <div id="session-label">{sessionTime}</div>
+          <button onClick={() => increaseSessionTime()}>+</button>
+        </div>
+        <div className="control-time">
+          <h3>break time</h3>
+          <button onClick={() => decreaseBreakTime()}>-</button>
+          <div id="break-label">{breakTime}</div>
+          <button onClick={() => increaseBreakTime()}>+</button>
+        </div>
+      </div>
+      <div id="display">
+        <h2>25:00</h2>
+      </div>
+    </body>
   );
 }
 
