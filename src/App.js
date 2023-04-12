714ee7b635008040f4436a1685c24435f476efd5
Wallace Nascimento <wallacediogo10@hotmail.com>
2023-04-12 08:31:57 -0300

    feature: basic logic and components created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   const [breakTime, setBreakTime] = useState(5);
   const [sessionTime, setSessionTime] = useState(25);
+  const [start, setStart] = useState(false)
 
   function increaseBreakTime() {
     setBreakTime(breakTime + 1)
@@ -26,20 +27,21 @@ function App() {
       <h1>POMODORO</h1>
       <div class="controls">
         <div className="control-time">
-          <h3>session time</h3>
-          <button onClick={() => decreaseSessionTime()}>-</button>
-          <div id="session-label">{sessionTime}</div>
-          <button onClick={() => increaseSessionTime()}>+</button>
+          <h3 id="session-label">session time</h3>
+          <button onClick={() => decreaseSessionTime()} id="session-decrement">-</button>
+          <div id="session-length">{sessionTime}</div>
+          <button onClick={() => increaseSessionTime()} id="session-increment">+</button>
         </div>
         <div className="control-time">
-          <h3>break time</h3>
-          <button onClick={() => decreaseBreakTime()}>-</button>
-          <div id="break-label">{breakTime}</div>
-          <button onClick={() => increaseBreakTime()}>+</button>
+          <h3 id="break-label">break time</h3>
+          <button onClick={() => decreaseBreakTime()} id="break-decrement">-</button>
+          <div id="break-length">{breakTime}</div>
+          <button onClick={() => increaseBreakTime()} id="break-increment">+</button>
         </div>
       </div>
       <div id="display">
-        <h2>25:00</h2>
+        <h2>{sessionTime}:00</h2>
+        <button onClick={() => setStart(!start)}>{start ? "Start" : "Stop"}</button>
       </div>
     </body>
   );
