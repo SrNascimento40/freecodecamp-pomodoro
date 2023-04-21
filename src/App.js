51f8c450c336ff3b3423f7bd6a70a26d11490346
Wallace Nascimento <wallacediogo10@hotmail.com>
2023-04-21 15:43:32 -0300

    fix: fix display and useEffect error

--- src/App.js
+++ src/App.js
@@ -1,38 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [breakTime, setBreakTime] = useState(5*60);
-  const [seconds, setSeconds] = useState(0);
-  const [sessionTime, setSessionTime] = useState(25*60);
-  const [start, setStart] = useState(false)
+  const [breakTime, setBreakTime] = useState(5 * 60);
+  const [sessionTime, setSessionTime] = useState(25 * 60);
+  const [displayTime, setDisplayTime] = useState(25 * 60);
+  const [start, setStart] = useState(false);
+  const [pause, setPause] = useState(false);
 
   function increaseBreakTime() {
-    setBreakTime(breakTime + 60)
+    setBreakTime(breakTime + 10);
   }
 
   function decreaseBreakTime() {
-    setBreakTime(breakTime - 60)
+    if (breakTime < 5) {
+      alert("tempo mínimo atingido!");
+      return;
+    }
+    setBreakTime(breakTime - 10);
   }
 
   function increaseSessionTime() {
-    setSessionTime(sessionTime + 60)
+    setSessionTime(sessionTime + 10);
+    if (!start) {
+      setDisplayTime(sessionTime + 10);
+    }
   }
 
   function decreaseSessionTime() {
-    setSessionTime(sessionTime - 60)
+    if (sessionTime < 5) {
+      alert("tempo mínimo atingido!");
+      return;
+    }
+    setSessionTime(sessionTime - 10);
+    if (!start) {
+      setDisplayTime(sessionTime - 10);
+    }
   }
 
-  useEffect(() => {
-    let intervalId = null;
-    while (start) {
-      intervalId = setInterval(() => {
-        setSeconds(prevSeconds => prevSeconds - 1);
-      }, 1000);
+  function formatTime(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  }
+
+  function resetTimer() {
+    if (start) {
+      setDisplayTime(sessionTime);
+      setStart(false);
+    } else {
+      setSessionTime(25 * 60);
+      setDisplayTime(25 * 60);
+      setBreakTime(5 * 60);
     }
-    return () => clearInterval(intervalId);
-  }, [start]);
-  
+  }
+
+  function controlTime() {
+    setStart(!start);
+    console.log("banana 1");
+    if (displayTime <= 0 && start) {
+      console.log("banana 2");
+      setStart(false);
+      setPause(true);
+      setDisplayTime(breakTime);
+    }
+    if (displayTime <= 0 && pause) {
+      console.log("banana 3");
+      setStart(true);
+      setPause(false);
+      setDisplayTime(sessionTime);
+    }
+  }
 
   return (
     <body>
@@ -40,21 +82,37 @@ function App() {
       <div class="controls">
         <div className="control-time">
           <h3 id="session-label">session time</h3>
-          <button onClick={() => decreaseSessionTime()} id="session-decrement">-</button>
-          <div id="session-length">{sessionTime/60}</div>
-          <button onClick={() => increaseSessionTime()} id="session-increment">+</button>
+          <button onClick={() => decreaseSessionTime()} id="session-decrement">
+            -
+          </button>
+          <div id="session-length">{formatTime(sessionTime)}</div>
+          <button onClick={() => increaseSessionTime()} id="session-increment">
+            +
+          </button>
         </div>
         <div className="control-time">
           <h3 id="break-label">break time</h3>
-          <button onClick={() => decreaseBreakTime()} id="break-decrement">-</button>
-          <div id="break-length">{breakTime/60}</div>
-          <button onClick={() => increaseBreakTime()} id="break-increment">+</button>
+          <button onClick={() => decreaseBreakTime()} id="break-decrement">
+            -
+          </button>
+          <div id="break-length">{formatTime(breakTime)}</div>
+          <button onClick={() => increaseBreakTime()} id="break-increment">
+            +
+          </button>
         </div>
       </div>
       <div id="display">
-        <h2 id="time-left">{sessionTime/60}:{seconds.toString().padStart(2, '0')}</h2>
-        <button className="controlPomo" id="start_stop" onClick={() => setStart(!start)}>{start ? "Start" : "Stop"}</button>
-        <button className="controlPomo" id="reset" onClick={() => setSessionTime(25)}>Reset</button>
+        <h2 id="time-left">{formatTime(displayTime)}</h2>
+        <button
+          className="controlPomo"
+          id="start_stop"
+          onClick={() => controlTime()}
+        >
+          {start ? "Stop" : "Start"}
+        </button>
+        <button className="controlPomo" id="reset" onClick={() => resetTimer()}>
+          Reset
+        </button>
       </div>
     </body>
   );
